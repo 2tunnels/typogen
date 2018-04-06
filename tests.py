e070aba187d1ecb8be4ba63d6242e50f785c0f6e
Vlad Dm <2tunnels@gmail.com>
2018-04-06 15:03:47 +0300

    add nearest_keys function

--- tests.py
+++ tests.py
@@ -137,3 +137,149 @@ class TestSkipSpaces:
         }
         assert typogen.skip_spaces('   cat   ') == set()
         assert typogen.skip_spaces('   ') == set()
+
+
+class TestNearestKeys:
+    def test_1(self):
+        assert typogen.nearest_keys('1') == ['2', 'q']
+        assert typogen.nearest_keys(1) == ['2', 'q']
+
+    def test_2(self):
+        assert typogen.nearest_keys('2') == ['3', 'w', 'q', '1']
+        assert typogen.nearest_keys(2) == ['3', 'w', 'q', '1']
+
+    def test_3(self):
+        assert typogen.nearest_keys('3') == ['4', 'e', 'w', '2']
+        assert typogen.nearest_keys(3) == ['4', 'e', 'w', '2']
+
+    def test_4(self):
+        assert typogen.nearest_keys('4') == ['5', 'r', 'e', '3']
+        assert typogen.nearest_keys(4) == ['5', 'r', 'e', '3']
+
+    def test_5(self):
+        assert typogen.nearest_keys('5') == ['6', 't', 'r', '4']
+        assert typogen.nearest_keys(5) == ['6', 't', 'r', '4']
+
+    def test_6(self):
+        assert typogen.nearest_keys('6') == ['7', 'y', 't', '5']
+        assert typogen.nearest_keys(6) == ['7', 'y', 't', '5']
+
+    def test_7(self):
+        assert typogen.nearest_keys('7') == ['8', 'u', 'y', '6']
+        assert typogen.nearest_keys(7) == ['8', 'u', 'y', '6']
+
+    def test_8(self):
+        assert typogen.nearest_keys('8') == ['9', 'i', 'u', '7']
+        assert typogen.nearest_keys(8) == ['9', 'i', 'u', '7']
+
+    def test_9(self):
+        assert typogen.nearest_keys('9') == ['0', 'o', 'i', '8']
+        assert typogen.nearest_keys(9) == ['0', 'o', 'i', '8']
+
+    def test_0(self):
+        assert typogen.nearest_keys('0') == ['p', 'o', '9']
+        assert typogen.nearest_keys(0) == ['p', 'o', '9']
+
+    def test_q(self):
+        assert typogen.nearest_keys('q') == ['1', '2', 'w', 's', 'a']
+        assert typogen.nearest_keys('Q') == ['1', '2', 'W', 'S', 'A']
+
+    def test_w(self):
+        assert typogen.nearest_keys('w') == ['q', '2', '3', 'e', 'd', 's', 'a']
+        assert typogen.nearest_keys('W') == ['Q', '2', '3', 'E', 'D', 'S', 'A']
+
+    def test_e(self):
+        assert typogen.nearest_keys('e') == ['w', '3', '4', 'r', 'f', 'd', 's']
+        assert typogen.nearest_keys('E') == ['W', '3', '4', 'R', 'F', 'D', 'S']
+
+    def test_r(self):
+        assert typogen.nearest_keys('r') == ['e', '4', '5', 't', 'g', 'f', 'd']
+        assert typogen.nearest_keys('R') == ['E', '4', '5', 'T', 'G', 'F', 'D']
+
+    def test_t(self):
+        assert typogen.nearest_keys('t') == ['r', '5', '6', 'y', 'h', 'g', 'f']
+        assert typogen.nearest_keys('T') == ['R', '5', '6', 'Y', 'H', 'G', 'F']
+
+    def test_y(self):
+        assert typogen.nearest_keys('y') == ['t', '6', '7', 'u', 'j', 'h', 'g']
+        assert typogen.nearest_keys('Y') == ['T', '6', '7', 'U', 'J', 'H', 'G']
+
+    def test_u(self):
+        assert typogen.nearest_keys('u') == ['y', '7', '8', 'i', 'k', 'j', 'h']
+        assert typogen.nearest_keys('U') == ['Y', '7', '8', 'I', 'K', 'J', 'H']
+
+    def test_i(self):
+        assert typogen.nearest_keys('i') == ['u', '8', '9', 'o', 'l', 'k', 'j']
+        assert typogen.nearest_keys('I') == ['U', '8', '9', 'O', 'L', 'K', 'J']
+
+    def test_o(self):
+        assert typogen.nearest_keys('o') == ['i', '9', '0', 'p', 'l', 'k']
+        assert typogen.nearest_keys('O') == ['I', '9', '0', 'P', 'L', 'K']
+
+    def test_p(self):
+        assert typogen.nearest_keys('p') == ['o', '0', 'l']
+        assert typogen.nearest_keys('P') == ['O', '0', 'L']
+
+    def test_a(self):
+        assert typogen.nearest_keys('a') == ['q', 'w', 's', 'x', 'z']
+        assert typogen.nearest_keys('A') == ['Q', 'W', 'S', 'X', 'Z']
+
+    def test_s(self):
+        assert typogen.nearest_keys('s') == ['a', 'w', 'e', 'd', 'x', 'z']
+        assert typogen.nearest_keys('S') == ['A', 'W', 'E', 'D', 'X', 'Z']
+
+    def test_d(self):
+        assert typogen.nearest_keys('d') == ['s', 'e', 'r', 'f', 'c', 'x']
+        assert typogen.nearest_keys('D') == ['S', 'E', 'R', 'F', 'C', 'X']
+
+    def test_f(self):
+        assert typogen.nearest_keys('f') == ['d', 'r', 't', 'g', 'v', 'c']
+        assert typogen.nearest_keys('F') == ['D', 'R', 'T', 'G', 'V', 'C']
+
+    def test_g(self):
+        assert typogen.nearest_keys('g') == ['f', 't', 'y', 'h', 'b', 'v']
+        assert typogen.nearest_keys('G') == ['F', 'T', 'Y', 'H', 'B', 'V']
+
+    def test_h(self):
+        assert typogen.nearest_keys('h') == ['g', 'y', 'u', 'j', 'n', 'b']
+        assert typogen.nearest_keys('H') == ['G', 'Y', 'U', 'J', 'N', 'B']
+
+    def test_j(self):
+        assert typogen.nearest_keys('j') == ['h', 'u', 'i', 'k', 'm', 'n']
+        assert typogen.nearest_keys('J') == ['H', 'U', 'I', 'K', 'M', 'N']
+
+    def test_k(self):
+        assert typogen.nearest_keys('k') == ['j', 'i', 'o', 'l', 'm']
+        assert typogen.nearest_keys('K') == ['J', 'I', 'O', 'L', 'M']
+
+    def test_l(self):
+        assert typogen.nearest_keys('l') == ['k', 'o', 'p']
+        assert typogen.nearest_keys('L') == ['K', 'O', 'P']
+
+    def test_z(self):
+        assert typogen.nearest_keys('z') == ['a', 's', 'x']
+        assert typogen.nearest_keys('Z') == ['A', 'S', 'X']
+
+    def test_x(self):
+        assert typogen.nearest_keys('x') == ['z', 's', 'd', 'c']
+        assert typogen.nearest_keys('X') == ['Z', 'S', 'D', 'C']
+
+    def test_c(self):
+        assert typogen.nearest_keys('c') == ['x', 'd', 'f', 'v']
+        assert typogen.nearest_keys('C') == ['X', 'D', 'F', 'V']
+
+    def test_v(self):
+        assert typogen.nearest_keys('v') == ['c', 'f', 'g', 'b']
+        assert typogen.nearest_keys('V') == ['C', 'F', 'G', 'B']
+
+    def test_b(self):
+        assert typogen.nearest_keys('b') == ['v', 'g', 'h', 'n']
+        assert typogen.nearest_keys('B') == ['V', 'G', 'H', 'N']
+
+    def test_n(self):
+        assert typogen.nearest_keys('n') == ['b', 'h', 'j', 'm']
+        assert typogen.nearest_keys('N') == ['B', 'H', 'J', 'M']
+
+    def test_m(self):
+        assert typogen.nearest_keys('m') == ['n', 'j', 'k']
+        assert typogen.nearest_keys('M') == ['N', 'J', 'K']
--- typogen.py
+++ typogen.py
@@ -47,7 +47,7 @@ def reverse_letters(keyword):
     return set(typos)
 
 
-def skip_spaces(keyword: str):
+def skip_spaces(keyword):
     keyword = keyword.strip()
     keyword = re.sub(r'\s+', ' ', keyword)
 
@@ -66,3 +66,58 @@ def skip_spaces(keyword: str):
         start = index + 1
 
     return set(typos)
+
+
+_nearest_keys = {
+    '1': ['2', 'q'],
+    '2': ['3', 'w', 'q', '1'],
+    '3': ['4', 'e', 'w', '2'],
+    '4': ['5', 'r', 'e', '3'],
+    '5': ['6', 't', 'r', '4'],
+    '6': ['7', 'y', 't', '5'],
+    '7': ['8', 'u', 'y', '6'],
+    '8': ['9', 'i', 'u', '7'],
+    '9': ['0', 'o', 'i', '8'],
+    '0': ['p', 'o', '9'],
+    'q': ['1', '2', 'w', 's', 'a'],
+    'w': ['q', '2', '3', 'e', 'd', 's', 'a'],
+    'e': ['w', '3', '4', 'r', 'f', 'd', 's'],
+    'r': ['e', '4', '5', 't', 'g', 'f', 'd'],
+    't': ['r', '5', '6', 'y', 'h', 'g', 'f'],
+    'y': ['t', '6', '7', 'u', 'j', 'h', 'g'],
+    'u': ['y', '7', '8', 'i', 'k', 'j', 'h'],
+    'i': ['u', '8', '9', 'o', 'l', 'k', 'j'],
+    'o': ['i', '9', '0', 'p', 'l', 'k'],
+    'p': ['o', '0', 'l'],
+    'a': ['q', 'w', 's', 'x', 'z'],
+    's': ['a', 'w', 'e', 'd', 'x', 'z'],
+    'd': ['s', 'e', 'r', 'f', 'c', 'x'],
+    'f': ['d', 'r', 't', 'g', 'v', 'c'],
+    'g': ['f', 't', 'y', 'h', 'b', 'v'],
+    'h': ['g', 'y', 'u', 'j', 'n', 'b'],
+    'j': ['h', 'u', 'i', 'k', 'm', 'n'],
+    'k': ['j', 'i', 'o', 'l', 'm'],
+    'l': ['k', 'o', 'p'],
+    'z': ['a', 's', 'x'],
+    'x': ['z', 's', 'd', 'c'],
+    'c': ['x', 'd', 'f', 'v'],
+    'v': ['c', 'f', 'g', 'b'],
+    'b': ['v', 'g', 'h', 'n'],
+    'n': ['b', 'h', 'j', 'm'],
+    'm': ['n', 'j', 'k'],
+}
+
+
+def nearest_keys(key):
+    key = str(key)
+    normalized_key = key.lower()
+
+    try:
+        keys = _nearest_keys[normalized_key]
+    except KeyError:
+        return key
+
+    if key.isupper():
+        return [k.upper() for k in keys]
+
+    return keys
